7cb1047b447c361359051d28bc385dcd475f461c
Júlio César <102155736+Kintiz@users.noreply.github.com>
2022-05-25 15:14:03 -0300

    Update Telas.java

--- src/interfaces/Telas.java
+++ src/interfaces/Telas.java
@@ -2,6 +2,7 @@ package interfaces;
 public interface Telas {
     public void tela();
     public void mensagem();
+    public void print();
     public String menu();
     public void crud();
     public void informacoes();
